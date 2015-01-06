f4f0357df02ce06999d6c93bab60f935586ae13b
Flycoding <Flycoding@yeah.net>
2015-01-06 09:38:37 +0800

    change token tag implementation
    Signed-off-by: Flycoding <Flycoding@yeah.net>

--- src/main/java/com/flyingh/tags/TokenTag.java
+++ src/main/java/com/flyingh/tags/TokenTag.java
@@ -1,41 +1,19 @@
 package com.flyingh.tags;
 
 import java.io.IOException;
-import java.io.Writer;
 import java.util.UUID;
 
 import javax.servlet.jsp.JspContext;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
-import javax.servlet.jsp.tagext.JspFragment;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 public class TokenTag extends SimpleTagSupport {
 	@Override
 	public void doTag() throws JspException, IOException {
-		getJspBody().invoke(null);
-	}
-
-	@Override
-	public void setJspContext(JspContext jspContext) {
-		super.setJspContext(jspContext);
-		setJspBody(new JspFragment() {
-
-			@Override
-			public void invoke(Writer out) throws JspException, IOException {
-				if (out == null) {
-					invoke(jspContext.getOut());
-				} else {
-					final UUID uuid = UUID.randomUUID();
-					jspContext.setAttribute("token", uuid.toString(), PageContext.SESSION_SCOPE);
-					out.write(String.format("<input type='hidden' name='token' value='%s'>", uuid));
-				}
-			}
-
-			@Override
-			public JspContext getJspContext() {
-				return jspContext;
-			}
-		});
+		final JspContext jspContext = getJspContext();
+		final UUID uuid = UUID.randomUUID();
+		jspContext.setAttribute("token", uuid.toString(), PageContext.SESSION_SCOPE);
+		jspContext.getOut().write(String.format("<input type='hidden' name='token' value='%s'>", uuid));
 	}
 }
